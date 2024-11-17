edb85b49bdff4d97e0e3babcbe825b39351586a0
Sarthak Patel <201901@daiict.ac.in>
2024-11-17 19:02:25 +0530

    Improvements

--- midi_generator.py
+++ midi_generator.py
@@ -8,11 +8,10 @@ import re
 
 # Define and initialize the dictionary with musical notes mapping    
 key_offset_dict = {
-    'G': -5, 'G#': -4, 'A': -3, 'A#': -2,
-    'B': -1, 'C': 0, 'C#': 1, 'D': 2,
-    'D#': 3, 'E': 4, 'F': 5, 'F#': 6
+    'C': 0, 'C#': 1, 'D': 2, 'D#': 3,
+    'E': 4, 'F': 5, 'F#': 6, 'G': 7,
+    'G#': 8, 'A': 9, 'A#': 10, 'B': 11
 }
-c_flute_notes_pitches = [43,45,47, 48, 50, 52, 53, 55, 57, 59, 60,62,64,65,67]
 
 sargam_notation_pitch_offset_dict = {
     ".pa": -5,
@@ -42,22 +41,29 @@ sargam_notation_pitch_offset_dict = {
     "pa.": 19
 }
 
+gap_note = "-"
+last_note_length = 5
+
 all_sargem_notes_list = list(sargam_notation_pitch_offset_dict.keys())
 all_scale_notes_list = list(key_offset_dict.keys())
+
+octaves = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 
 major_scale_notes = [0, 2, 4, 5, 7, 9, 10, 12, 14, 16, 17, 19, 21, 22, 24]
 major_scale_root_note = 5
 
+generation_type_cycle = "Cycle"
 
-def create_midi_file(input_key, input_notations, smallest_beat_length):
+
+def create_midi_file(input_key, input_notations, smallest_beat_length, input_octave, step_length, no_of_cycles, type_of_generation):
     
     smallest_beat_length = float(smallest_beat_length)
     input_notations = input_notations.lower()
     input_key = input_key.capitalize()
     key_offset = key_offset_dict[input_key]
     
-    mf = get_mf(input_notations, smallest_beat_length, key_offset)
+    mf = get_mf(input_notations, smallest_beat_length, key_offset, input_octave, step_length, no_of_cycles, type_of_generation)
 
     file_path = "output.mid"
     
@@ -73,6 +79,8 @@ def create_midi_file(input_key, input_notations, smallest_beat_length):
 
 
 def get_avroh_note(note):
+    if (note == gap_note):
+        return gap_note
     note_index = all_sargem_notes_list.index(note)
     higher_octave_root_note = major_scale_notes.index(all_sargem_notes_list.index("sa."))
     root_note = major_scale_notes.index(all_sargem_notes_list.index("sa"))
@@ -82,12 +90,16 @@ def get_avroh_note(note):
     return all_sargem_notes_list[major_scale_notes[new_note_index]]
 
 
-def get_mf(input_notations, smallest_beat_length, key_offset):
+def get_mf(input_notations, smallest_beat_length, key_offset, input_ocatve, step_length, no_of_cycles, type_of_generation):
 
-    current_scale_flute_notes_pitches = {value: (sargam_notation_pitch_offset_dict[value] + 60 + key_offset) for value in sargam_notation_pitch_offset_dict}
+    octave_offset = input_ocatve + 1
+    pitch_offset = 12 * octave_offset
+
+    current_scale_flute_notes_pitches = {value: (sargam_notation_pitch_offset_dict[value] + pitch_offset + key_offset) for value in sargam_notation_pitch_offset_dict}
 
     input_sargam = re.sub(r'[0-9]', '', input_notations).split()
     input_note_lengths = re.sub(r'[^0-9 ]', "0", input_notations).split()
+    input_note_lengths = [(1 if float(note_length) == 0 else float(note_length)) for note_length in input_note_lengths]
     
     # create your MIDI object
     channel = 0
@@ -102,13 +114,15 @@ def get_mf(input_notations, smallest_beat_length, key_offset):
     final_input_sargam = []
     final_input_note_lengths = []
 
-    step_length = int(step_length_text_box.get("1.0", "end-1c"))
-
-    if (radio_value.get() == radio_option_cycle):
-        for cycle_index in range(0, int(cycle_repetition_text_box.get("1.0", "end-1c"))):
+    if (type_of_generation == radio_option_cycle):
+        for cycle_index in range(0, no_of_cycles):
             for index in range(0, len(input_sargam)):
 
                 initial_sargam_note = input_sargam[index] # eg "sa"
+
+                if (initial_sargam_note == gap_note):
+                    final_input_sargam.append(gap_note)
+                    continue
 
                 initial_note_index = all_sargem_notes_list.index(initial_sargam_note)
                 sargam_note = all_sargem_notes_list[major_scale_notes[major_scale_notes.index(initial_note_index) + (cycle_index*step_length)]]
@@ -120,10 +134,14 @@ def get_mf(input_notations, smallest_beat_length, key_offset):
 
         avroh_sargam = [get_avroh_note(input_sargam_note_temp) for input_sargam_note_temp in input_sargam]
 
-        for cycle_index in range(0, int(cycle_repetition_text_box.get("1.0", "end-1c"))):
+        for cycle_index in range(0, no_of_cycles):
             for index in range(0, len(avroh_sargam)):
 
                 initial_sargam_note = avroh_sargam[index] # eg "sa"
+
+                if (initial_sargam_note == gap_note):
+                    final_input_sargam.append(gap_note)
+                    continue
 
                 initial_note_index = all_sargem_notes_list.index(initial_sargam_note)
                 sargam_note = all_sargem_notes_list[major_scale_notes[major_scale_notes.index(initial_note_index) - (cycle_index*step_length)]]
@@ -137,24 +155,20 @@ def get_mf(input_notations, smallest_beat_length, key_offset):
         final_input_note_lengths = input_note_lengths
 
 
-    final_input_note_lengths[len(final_input_note_lengths) - 1] = smallest_beat_length * 8
-
-    print(final_input_sargam)
-    print(final_input_note_lengths)
-
+    final_input_note_lengths[len(final_input_note_lengths) - 1] = last_note_length*final_input_note_lengths[len(final_input_note_lengths) - 1]
 
     for index in range(0, len(final_input_sargam)):
         sargam_note = final_input_sargam[index] # eg "sa"
 
-        duration = int(final_input_note_lengths[index])
+        duration = float(final_input_note_lengths[index])
         if (duration == 0):
-            duration = 1
-        duration = smallest_beat_length * duration
+            duration = smallest_beat_length
+        else:
+            duration = smallest_beat_length * duration
 
-        if (sargam_note == "_"):
+        if (sargam_note == gap_note):
             beat_index += duration
             continue
-
 
         time = beat_index
         pitch = int(current_scale_flute_notes_pitches[sargam_note])
@@ -167,15 +181,15 @@ def get_mf(input_notations, smallest_beat_length, key_offset):
 
 
 def generate_midi_file():
-    try:
-        input_scale = str(scale_entry_box.get())
-        input_notations = str(input_notation_entry_box.get("1.0", "end-1c"))
-        smallest_beat_length = str(smallest_beat_length_entry_box.get("1.0", "end-1c"))
-        create_midi_file(input_scale, input_notations, smallest_beat_length)
+    input_scale = str(scale_entry_box.get())
+    input_notations = str(input_notation_entry_box.get("1.0", "end-1c"))
+    smallest_beat_length = str(smallest_beat_length_entry_box.get("1.0", "end-1c"))
+    input_octave = int(octave_dropdown.get())
+    step_length = int(step_length_text_box.get("1.0", "end-1c"))
+    no_of_cycles = int(cycle_repetition_text_box.get("1.0", "end-1c"))
+    type_of_generation = str(radio_value.get())
+    create_midi_file(input_scale, input_notations, smallest_beat_length, input_octave, step_length, no_of_cycles, type_of_generation)
 
-    except ValueError:
-        print(ValueError)
-        pass
 
 # Create the main window
 root = tk.Tk()
@@ -193,7 +207,11 @@ def handle_reverse_tab(event: tk.Event):
     return "break"
 
 def handle_focus(event: tk.Event):
-    event.widget.tag_add("sel", "1.0", "end-1c")
+    if (type(event.widget) == ttk.Combobox): 
+        event.widget.select_range(0, 'end')
+    elif (type(event.widget) == tk.Text):
+        event.widget.tag_add("sel", "1.0", "end-1c")
+
 
 def do_common_things_to_text_box(text_box: tk.Text):
     text_box.bind("<Tab>", handle_tab)
@@ -203,15 +221,26 @@ def do_common_things_to_text_box(text_box: tk.Text):
 # Input field for Key
 style = ttk.Style()
 style.configure("TCombobox", font=("Arial", 30))  # Change text size to 14
-tk.Label(root, text="Key (eg \"A#\")", font=my_font).grid(row=0, column=0, pady=(y_gap, 0))
-selected_key_option = tk.StringVar(value="G")
+
+gap_key_octave = 300
+
+x_padding_key = (0, gap_key_octave)
+tk.Label(root, text="Key", font=my_font).grid(row=0, column=0, pady=(y_gap, 0), padx=x_padding_key)
 scale_entry_box = ttk.Combobox(root, values=all_scale_notes_list)
-scale_entry_box.set(all_scale_notes_list[0])
+scale_entry_box.set(all_scale_notes_list[7])
 scale_entry_box.config(font=my_font)
 do_common_things_to_text_box(scale_entry_box)
-scale_entry_box.grid(row=1, column=0)
+scale_entry_box.grid(row=1, column=0, padx=x_padding_key)
 
-tk.Label(root, text="Notation (eg: \"sa re1 _.ni _ sa4\")", font=my_font).grid(row=2, column=0, pady=(y_gap, 0))
+x_padding_octave = (gap_key_octave, 0)
+tk.Label(root, text="Octave", font=my_font).grid(row=0, column=0, pady=(y_gap, 0), padx=x_padding_octave)
+octave_dropdown = ttk.Combobox(root, values=octaves)
+octave_dropdown.set(4)
+octave_dropdown.config(font=my_font)
+do_common_things_to_text_box(octave_dropdown)
+octave_dropdown.grid(row=1, column=0, padx=x_padding_octave)
+
+tk.Label(root, text="Notation (eg: \"sa re1 _.ni - sa4\")", font=my_font).grid(row=2, column=0, pady=(y_gap, 0))
 input_notation_entry_box = tk.Text(root, font=my_font, height=5, undo=True)
 input_notation_entry_box.focus()
 do_common_things_to_text_box(input_notation_entry_box)
@@ -223,8 +252,10 @@ smallest_beat_length_entry_box.insert("1.0", "1")
 do_common_things_to_text_box(smallest_beat_length_entry_box)
 smallest_beat_length_entry_box.grid(row=5, column=0)
 
+
+
 radio_option_normal = "Normal"
-radio_option_cycle = "Cycle"
+radio_option_cycle = generation_type_cycle
 radio_value = tk.StringVar(value=radio_option_cycle)
 
 # Create two radio buttons
@@ -249,7 +280,7 @@ radio2 = tk.Radiobutton(root, text=radio_option_cycle, variable=radio_value, val
 radio1.grid(row=7, column=0, pady=(y_gap, 0))  # Align to the left
 radio2.grid(row=8, column=0)
 
-cycle_repeatition_label = tk.Label(root, text="Number of cycles (eg: \"5\")", font=my_font)
+cycle_repeatition_label = tk.Label(root, text="Number of cycles (eg: \"5\") (<= 14)", font=my_font)
 cycle_repeatition_label.grid(row=9, column=0, pady=(y_gap, 0))
 cycle_repetition_text_box = tk.Text(root, height=1, font=my_font, undo=True)
 cycle_repetition_text_box.insert("1.0", "8")
